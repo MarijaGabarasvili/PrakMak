61574935930fd376cd776033e09552601c196887
marij <marija.gabarasvili@gmail.com>
2025-02-28 00:14:03 +0200

    did some changes

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import PySimpleGUI as sg
 import random
 
-length = random.choice([15, 25])
-binary_sequence = " ".join(str(random.choice([0, 1])) for _ in range(length))
+layout = [
+    [sg.Text("", size=(16, 1)), sg.Button("Player 1"), sg.Text("", size=(33, 1)), sg.Text("", size=(15, 1)), sg.Button("Player 2"), sg.Text("", size=(15, 1))],
+    [sg.Text("Enter the length of the binary sequence:",font=("Helvetica", 20)), sg.InputText(key='-LENGTH-')],
+    [sg.Text("", size=(40, 1)), sg.Button("Generate", size=(20, 2))],
+    [sg.Text("", size=(15, 1)), sg.Button("Algorithm 1"), sg.Text("", size=(30, 1)), sg.Text("", size=(15, 1)), sg.Button("Algorithm 2"), sg.Text("", size=(15, 1))],
 
-layout = [[sg.Text(binary_sequence)]]
-window = sg.Window("Binary Grid", layout)
+    [sg.Text("", key='-OUTPUT-')]
+]
+
+window = sg.Window("Binary Sequence Generator", layout)
+
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED:
         break
+    if event == "Generate":
+        try:
+            length = int(values['-LENGTH-'])
+            if length <= 14 or length >= 26:
+                window['-OUTPUT-'].update("Please enter a number between 15 and 25.")
+                continue
+            line = " ".join(str(random.choice([0, 1])) for _ in range(length))
+            window['-OUTPUT-'].update(line)
+        except ValueError:
+            window['-OUTPUT-'].update("Please enter a number between 15 and 25.")
 
 window.close()
